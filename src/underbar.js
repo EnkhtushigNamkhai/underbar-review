0543c33894c6bddf9622fe9ba991f3c0dba7fa66
Hack Reactor Student <student@hackreactor.com>
2017-10-10 13:25:02 -0700

    part1 in progress, finished uniq

--- src/underbar.js
+++ src/underbar.js
@@ -7,6 +7,8 @@
   // seem very useful, but remember it--if a function needs to provide an
   // iterator when the user does not pass one in, this will be handy.
   _.identity = function(val) {
+
+    return val;
   };
 
   /**
@@ -37,6 +39,13 @@
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   _.last = function(array, n) {
+    if (n === undefined) {
+      return array[array.length - 1];
+    } else if (n > array.length) {
+      return array;
+    } else {
+      return array.slice(array.length - n);
+    }    
   };
 
   // Call iterator(value, key, collection) for each element of collection.
@@ -45,6 +54,16 @@
   // Note: _.each does not have a return value, but rather simply runs the
   // iterator function over each item in the input collection.
   _.each = function(collection, iterator) {
+    if (Array.isArray(collection)) {    
+      for (var i = 0; i < collection.length; i++) {
+        iterator(collection[i], i, collection);
+      }
+    } else {
+      for (var key in collection) {
+        iterator(collection[key], key, collection);
+      }
+    }
+    
   };
 
   // Returns the index at which value can be found in the array, or -1 if value
@@ -66,24 +85,78 @@
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+    var result = [];
+    // for (var i = 0; i < collection.length; i++) {
+    //   if (test(collection[i])) {
+    //     result.push(collection[i]);  
+    //   }
+    // }
+    _.each(collection, function(value, key, arr) {
+      if (test(value)) {
+        result.push(value);
+      }
+    });
+
+    return result;
   };
 
   // Return all elements of an array that don't pass a truth test.
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
+    var result = [];
+
+    _.each(collection, function(value, key, arr) {
+      if (!test(value)) {
+        result.push(value);
+      }
+    });
+    
+    return result;
   };
 
   // Produce a duplicate-free version of the array.
   _.uniq = function(array, isSorted, iterator) {
+    if (iterator === undefined) {
+      var result = [];
+    
+      for (var i = 0; i < array.length; i++) {
+        if (result.includes(array[i])) {
+          continue;
+        }
+        result.push(array[i]);
+      }
+      
+      return result;
+    } else {
+      var result = [];
+      var evaluatedUniqueness = [];
+      
+      for ( var i = 0; i < array.length; i++) {
+        var value = iterator(array[i]);
+        if (!evaluatedUniqueness.includes(value)) {
+          result.push(array[i]);
+          evaluatedUniqueness.push(value);
+        }  
+      }
+      return result;
+
+    }
+    
+    //first check what the array value gets evaluated to
+    //if the value is unique, not in evaluatedUniqueness = []
+      //push the item to result and push value to evaluatedUniqueness
+    //return result
+     
+    
   };
 
 
   // Return the results of applying an iterator to each element.
-  _.map = function(collection, iterator) {
-    // map() is a useful primitive iteration function that works a lot
-    // like each(), but in addition to running the operation on all
+  _.map = function(collection, iterator) {                                                         
+    // map() is a useful primitive iteration function that works a lot      
     // the members, it also maintains an array of results.
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
   };
 
   /*
